dc5f3f926c698723b0483be7fcc29e237245cffd
奶糖星球 <2623040723@qq.com>
2026-01-09 20:21:48 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import path from 'path' // 引入path模块
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 关键：base = /Gitee仓库名/（例如仓库名是recipe-collection，则写/recipe-collection/）
+  base: '/recipe-collection/', 
   resolve: {
     // 配置路径别名
     alias: {
